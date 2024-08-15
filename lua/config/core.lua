376e5bc91677bf220eb848a6579550e17159e692
Ubuntu <idtadmin@temp.net.local>
2024-08-15 13:47:34 +0700

    update

--- lua/config/core.lua
+++ lua/config/core.lua
@@ -1,5 +1,10 @@
 vim.cmd("set number")
 vim.cmd("set relativenumber")
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
 -- ANSIBLE/YAML --
 vim.filetype.add({
 	extension = {
