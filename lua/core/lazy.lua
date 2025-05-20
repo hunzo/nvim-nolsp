966cd76b6f81e81e0c24842362c199b3e60a6681
surapong.n <surapong.n@nida.ac.th>
2025-05-20 10:11:24 +0700

    update config

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -26,7 +26,6 @@ require("lazy").setup({
 	spec = {
 		-- import your plugins
 		{ import = "plugins" },
-		{ import = "lsp" },
 		{ import = "colorschemes" },
 	},
 	-- Configure any other settings here. See the documentation for more details.
