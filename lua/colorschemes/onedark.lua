596f294c20a10de7b700025461a9cc80ce060125
Setsuna F. Seiei <surapong.n@nida.ac.th>
2025-10-20 14:03:54 +0700

    update lazy.vim

--- lua/colorschemes/onedark.lua
+++ lua/colorschemes/onedark.lua
@@ -1,6 +1,6 @@
 return {
 	"navarasu/onedark.nvim",
-	priority = 999, -- make sure to load this before all the other start plugins
+	priority = 1000, -- make sure to load this before all the other start plugins
 	config = function()
 		require("onedark").setup({
 			style = "darker",
